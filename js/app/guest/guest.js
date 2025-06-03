0e074eedc53e1e9fac7b78902ae116284ab04cd6
Hafidz Alfatih <hafidzalfatih04@gmail.com>
2025-06-03 13:59:43 +0700

    update

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -247,10 +247,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'The Wedding of Fahmi and Nurul',
+            dates: `${formatDate('2025-06-29 10:00')}/${formatDate('2025-06-29 11:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            location: 'Kompleks Taman Lopang Indah Blok FU 19 No. 7, Kelurahan Lopang, Kecamatan Serang, Kota Serang, Provinsi Banten, Indonesia.',
             ctz: config.get('tz'),
         });
 
